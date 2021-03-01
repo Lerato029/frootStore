70b718923a195f1b262ef161266948f66325e967
Lerato029 <79574031+Lerato029@users.noreply.github.com>
2021-03-01 12:13:08 +0200

    h1 animation tranisition to 60px

--- js/jQuery.js
+++ js/jQuery.js
@@ -11,7 +11,7 @@ $(document).ready(function () {
       fontSize: '40px',
     }, 1000).animate({
       opacity: '1',
-      fontSize: '80px',
+      fontSize: '60px',
     }, 3000), animateH1(); //called back so it runs again
   })
 

40dcabcad7f81426c3be888471bc5a221505d769
Lerato029 <79574031+Lerato029@users.noreply.github.com>
2021-03-01 12:05:45 +0200

    line 14-transition to 60px

--- js/jQuery.js
+++ js/jQuery.js
@@ -11,7 +11,7 @@ $(document).ready(function () {
       fontSize: '30px',
     }, 1000).animate({
       opacity: '1',
-      fontSize: '50px',
+      fontSize: '60px',
     }, 3000), animateH1(); //called back so it runs again
   })
 
